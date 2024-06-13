fd54b57eda8176f5a911d10dd0f268859053b392
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-13 11:21:46 +0200

    items tilføjet

--- flutter_application_1/lib/TrashItemList.dart
+++ flutter_application_1/lib/TrashItemList.dart
@@ -9,10 +9,82 @@ final List<TrashItem> allItems = [
     tips: 'Smid eventuele madreesterne i kompost.',
   ),
   TrashItem(
-    name: 'Frugt',
+    name: 'Frugt og grøntsager',
     sortingType: 'Kompost',
     imageUrl: 'https://example.com/banana_peel.jpg',
     tips: 'kan bruges som kompost eller gødning',
+  ),
+  TrashItem(
+    name: 'Snavset papir',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Snavset pap',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Køkkenrulle',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Støvsugerposer',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Bleer',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Gavebånd',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Emballage der ikke kan skrabes rent',
+    sortingType: 'Restaffald',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Sovs og fedt',
+    sortingType: 'Kompost',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Pasta og ris',
+    sortingType: 'Kompost',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Ost',
+    sortingType: 'Kompost',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Brød og kager',
+    sortingType: 'Kompost',
+    imageUrl: '',
+    tips: '',
+  ),
+  TrashItem(
+    name: 'Teblade og kaffegrums',
+    sortingType: 'Kompost',
+    imageUrl: '',
+    tips: 'også filtre og poser',
   ),
   // Tilføj flere TrashItems her
 ];
