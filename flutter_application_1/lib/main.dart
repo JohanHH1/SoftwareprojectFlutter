b12467b1833092b0689df92d0890e57e0f32bcef
Kajsapixar <kajsa-2001@hotmail.com>
2024-06-13 13:45:26 +0200

    debug veck

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -16,10 +16,12 @@ runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Navigation Example',
       home: HomePage(),
     );
