277bba8a1d4d1c8b47d8bf78ef4bfddaf607b34e
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-13 13:23:38 +0200

    Firebase first update
    firebare sat under main og trashitem

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,10 +3,16 @@ import 'TrashItem.dart';
 import 'TrashItemList.dart';
 import 'navigation_bar.dart';
 import 'SearchPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Importer filen med Firebase konfigurationen
 
 
-void main() => runApp(const MyApp());
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); // Sikrer at bindinger er initialiseret før runApp
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);// Initialiser Firebase med de korrekte konfigurationsindstillinger
+runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
