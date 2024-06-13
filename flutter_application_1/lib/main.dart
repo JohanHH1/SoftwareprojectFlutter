e4e6a740b8f1b25a2c797bc4a8f468ae291b2b5b
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-13 13:16:23 +0200

    Revert "Firebase first update"
    This reverts commit 8760ca07b6a1e1ba4b620ec209006eed1f1e7827.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,19 +3,9 @@ import 'TrashItem.dart';
 import 'TrashItemList.dart';
 import 'navigation_bar.dart';
 import 'SearchPage.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // Importer filen med Firebase konfigurationen
 
 
-<<<<<<< Updated upstream
 void main() => runApp(const MyApp());
-=======
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized(); // Sikrer at bindinger er initialiseret før runApp
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);// Initialiser Firebase med de korrekte konfigurationsindstillinger
-runApp(const MyApp());
-}
->>>>>>> Stashed changes
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
