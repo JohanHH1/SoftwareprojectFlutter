4c8f40c1048d018cd808da7e0d4011e3015fb85b
JohanHH1 <johan.holm.hansen@gmail.com>
2024-06-13 11:22:59 +0200

    Working on LocationPAge

--- flutter_application_1/lib/navigation_bar.dart
+++ flutter_application_1/lib/navigation_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'SearchPage.dart';
 import 'main.dart';
+import 'LocationPage.dart';
 
 class NavigationBarr extends StatelessWidget {
   const NavigationBarr({super.key});
@@ -39,7 +40,10 @@ class NavigationBarr extends StatelessWidget {
             icon: const Icon(Icons.pin_drop, color: Color.fromARGB(255, 36, 100, 38)),
             tooltip: 'Map',
             onPressed: () {
-              // Add your onPressed logic here
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => const MapPage()),
+              );
             },
           ),
         ],
