e78909fc9a16e1692622bf58b9b65898633c8376
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-19 12:45:11 +0200

    Markers på kortet

--- flutter_application_1/lib/LocationPage.dart
+++ flutter_application_1/lib/LocationPage.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'TrashItem.dart';
 import 'TrashItemList.dart';
@@ -6,16 +8,59 @@ import 'SearchPage.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
 class LocationPage extends StatefulWidget{
   const LocationPage({super.key});
+  
 
   @override
   _MyLocationPageState createState() => _MyLocationPageState();
 }
 
 class _MyLocationPageState extends State<LocationPage> {
+  List<Marker> _markers = [];
+   String _errorMessage = '';
+   final MapController _mapController = MapController();
+
   @override
+  void initState() {
+    super.initState();
+    _addTrashMarkers();
+  }
+  
+Future<void> _addTrashMarkers() async {
+  log(6.0);
+   try {
+     final String jsonString = await rootBundle.loadString('export-3.geojson');
+     final jsonData = json.decode(jsonString);
+     final features = jsonData['features'];
+
+     setState(() {
+       _markers = (features as List).map((feature) {
+         final coordinates = feature['geometry']['coordinates'];
+         
+         return Marker(
+           width: 80.0,
+           height: 80.0,
+           point: LatLng(coordinates[1], coordinates[0]),
+           child: const Icon(
+             Icons.location_on,
+             color: Colors.green,
+             size: 40.0,
+           ),
+         );
+       }).toList();
+     });
+   } catch (e) {
+     setState(() {
+       _errorMessage = 'Error loading markers: $e';
+     });
+   }
+   
+ }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -29,21 +74,24 @@ class _MyLocationPageState extends State<LocationPage> {
   }
   Widget content() {
     return FlutterMap(
+      mapController: _mapController,
       options: const MapOptions(
         initialCenter: LatLng(55.676098, 12.568337),
         initialZoom: 11,
         interactionOptions:
           InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom), 
         ),
+        
       children: [
         openStreetMapTileLayer,
-        CurrentLocationLayer(),
-      ],
-    );
+        CurrentLocationLayer(), 
+          MarkerLayer(
+                   markers: _markers,
+                 ),],);   
   }
-  
-}
+
 TileLayer get openStreetMapTileLayer => TileLayer(
   urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+  //userAgentPackageName: 'dev.fleaflet.flutter_map.example',
 );
+}
