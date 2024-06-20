6c962ae39607ebb98965292f24ec2c089ecdfe76
JohanHH1 <johan.holm.hansen@gmail.com>
2024-06-20 12:41:16 +0200

    Revert "FLutter på mobilen og tilføjede kommentarer"
    This reverts commit 9d526fe66f1e7edb7895754881b75f9c378b0f23.

--- flutter_application_1/lib/database.dart
+++ flutter_application_1/lib/database.dart
@@ -12,14 +12,12 @@ class ItemList1 extends StatefulWidget {
 class _ItemList1State extends State<ItemList1> {
   List<Map<String, dynamic>> items = [];
   List<Map<String, dynamic>> filteredItems = [];
-  //Håndterer inputs i søgefeltet
   TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     fetchItems();
-    //En listener der kalder "filterItems" hver kan input ændrer sig i søgefeltet
     searchController.addListener(() {
       filterItems();
     });
@@ -27,38 +25,27 @@ class _ItemList1State extends State<ItemList1> {
 
   Future<void> fetchItems() async {
     try {
-      //Henter roden til vores firebase
       final storageRef = FirebaseStorage.instance.ref();
-      //Henter den specifikke fil
       final trashRef = storageRef.child("trashItems/trash_items.json");
 
-      //Henter vores data fra filen
       final data = await trashRef.getData();
-
-      //Checker at vi har noget data
       if (data != null) {
       
-        //Bruger jsonDecode fra convert packagen til at tage alt data og side i en list af dynamiske objekter,
-        //hvilket er meget nemt at arbejde med.
         final jsonData = jsonDecode(utf8.decode(data)) as List<dynamic>;
 
-        //JSON dataen starter som en liste med dynamiske objekter.
-        //Herefter tages listen og laves til en ny liste, hvor hvert
-        //element er et map med alt dataen om skraldet.
+        // Extract item details (assuming the JSON is a list of objects)
         final List<Map<String, dynamic>> itemDetails = jsonData.map((item) => item as Map<String, dynamic>).toList();
 
-        // Updaterer tilstanden 
+        // Update the state to display the items
         setState(() {
           items = itemDetails;
           filteredItems = itemDetails;
         });
       } else {
-        //Hvis der ikke er noget data
-        throw Exception('Kunne ikke hente JSON data');
+        throw Exception('Failed to load JSON data');
       }
     } catch (e) {
-      //Printer fejl
-      print('Der gik noget galt: $e');
+      print('Error retrieving or parsing data: $e');
     }
   }
 
@@ -122,6 +109,5 @@ class _ItemList1State extends State<ItemList1> {
   void dispose() {
     searchController.dispose();
     super.dispose();
-    print(items);
   }
 }
