711bdefaf8e0f263dea232d6e1f3253ed51f2982
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-21 09:19:58 +0200

    clean up

--- flutter_application_1/lib/LocationPage.dart
+++ flutter_application_1/lib/LocationPage.dart
@@ -88,19 +88,6 @@ class _MyHomePageState extends State<LocationPage> {
                ),
              ),
            );
-          //  } else if(choosen=='6'){
-          //  _markers6.add(
-          //    Marker(
-          //      width: 80.0,
-          //      height: 80.0,
-          //      point: firstCoordinate,
-          //      child: const Icon(
-          //        Icons.location_on,
-          //        color: Colors.blue,
-          //        size: 40.0,
-          //      ),
-          //    ),
-          //  );
             }  
        }
      }});
@@ -109,6 +96,7 @@ class _MyHomePageState extends State<LocationPage> {
        _errorMessage = 'Error loading markers: $e';
      });
    }
+   
   }
 
  @override
