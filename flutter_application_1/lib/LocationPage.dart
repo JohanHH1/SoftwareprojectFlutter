4aec072afa5ebb8baa3d11e4f8258f19cbe1d4a0
Lizette-N <88434774+Lizette-N@users.noreply.github.com>
2024-06-24 10:26:46 +0200

    Update LocationPage.dart

--- flutter_application_1/lib/LocationPage.dart
+++ flutter_application_1/lib/LocationPage.dart
@@ -26,8 +26,7 @@ class _MyHomePageState extends State<LocationPage> {
   }
 
   Future<void> _addAllMarkers() async {
-    await _addTrashMarkers(
-        '1'); // tilføjer alle røde genbrugspladsers markør fra fil 1
+    await _addTrashMarkers('1'); // tilføjer alle røde genbrugspladsers markør fra fil 1
     await _addTrashMarkers('2'); // tilføjer alle affalds containere fra fil 2
     setState(() {
       _errorMessage = ''; // fanger eventuelle fejl beskeder
@@ -36,16 +35,14 @@ class _MyHomePageState extends State<LocationPage> {
 
   // funktion til hvad der skal stå når man "tapper" på en genbrugsplads
   void _onMarkerTapped(LatLng position, String name, String openHours) {
-    showDialog(
-      // åbner et lille vindue foran kortet
+    showDialog( // åbner et lille vindue foran kortet
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(name), // displayer navn
           content: Text('Åbningstider: $openHours'), // displayer
           actions: [
-            TextButton(
-              // laver en knap til at forlade det lille vindue.
+            TextButton( // laver en knap til at forlade det lille vindue.
               child: const Text('OK'),
               onPressed: () {
                 Navigator.of(context).pop(); // fjerne vinduet fra skærmen
@@ -58,8 +55,7 @@ class _MyHomePageState extends State<LocationPage> {
   }
 
   Future<void> _addTrashMarkers(String choosen) async {
-    String export =
-        'assets/export-$choosen.geojson'; // definere en filsti til den valgte fil med tallet choosen
+    String export = 'assets/export-$choosen.geojson'; // definere en filsti til den valgte fil med tallet choosen
     try {
       final String jsonString =
           await rootBundle.loadString(export); // loader filen ind som string
@@ -69,35 +65,27 @@ class _MyHomePageState extends State<LocationPage> {
 
       setState(() {
         for (var feature in features) {
-          // går alle featuresne igennem en ad gangen.
           final geometry = feature['geometry']; // finder geometry i featuren
-          final coordinates =
-              geometry['coordinates']; //finder koordinaterne i geometry
-          final properties =
-              feature['properties']; // finder properties i featuren
+          final coordinates = geometry['coordinates']; //finder koordinaterne i geometry
+          final properties = feature['properties']; // finder properties i featuren
           LatLng? firstCoordinate;
           String? siteName;
           String? openHours;
-          siteName = properties['name'] ??
-              'Genbrugsplads'; //hvis der findes et navn så gem navnet, ellers 'genbrugsplads'
-          openHours = properties['opening_hours'] ??
-              'Ukendt'; //hvis der findes åbningstider så gem dem, ellers 'ukendt'
+          siteName = properties['name'] ?? 'Genbrugsplads'; //hvis der findes et navn så gem navnet, ellers 'genbrugsplads'
+          openHours = properties['opening_hours'] ?? 'Ukendt'; //hvis der findes åbningstider så gem dem, ellers 'ukendt'
 
-          if (geometry['type'] == 'Point') {
-            // hvis der kun er et koordinat så laver vi en markør i dette punkt
+          if (geometry['type'] == 'Point') { // hvis der kun er et koordinat så laver vi en markør i dette punkt
             firstCoordinate = LatLng(coordinates[1], coordinates[0]);
           } else if (geometry['type'] == 'LineString' ||
-              geometry['type'] == 'Polygon') {
-            // hvis der er flere koordinater
+              geometry['type'] == 'Polygon') { // hvis der er flere koordinater
             firstCoordinate = LatLng(
                 coordinates[0][0][1],
-                coordinates[0][0]
-                    [0]); //så tager vi det første og laver en markør der
+                coordinates[0][0][0]); //så tager vi det første og laver en markør der
           } //
-          if (firstCoordinate != null) {
-            // hvis der er en koorniat så fortsætter vi
+          if (firstCoordinate != null) { // hvis der er en koorniat så fortsætter vi
             if (choosen == '1') {
-              _markers1.add(Marker(
+              _markers1.add(
+                Marker(
                 width: 80.0,
                 height: 80.0,
                 point: firstCoordinate,
@@ -106,8 +94,7 @@ class _MyHomePageState extends State<LocationPage> {
                   onTap: () =>
                       _onMarkerTapped(firstCoordinate!, siteName!, openHours!),
                   child: const Icon(
-                    Icons
-                        .recycling, // de bliver vist som røde genbrugs symboler
+                    Icons.recycling, // de bliver vist som røde genbrugs symboler
                     color: Colors.red,
                     size: 35.0,
                   ),
@@ -115,8 +102,7 @@ class _MyHomePageState extends State<LocationPage> {
               ));
             } else if (choosen == '2') {
               _markers2.add(
-                Marker(
-                  // andet dokument bliver vist som små grønne skraldespande
+                Marker( // andet dokument bliver vist som små grønne skraldespande
                   width: 80.0,
                   height: 80.0,
                   point: firstCoordinate,
@@ -131,8 +117,7 @@ class _MyHomePageState extends State<LocationPage> {
           }
         }
       });
-    } catch (e) {
-      // try catch til at fange fejl
+    } catch (e) { // try catch til at fange fejl
       setState(() {
         _errorMessage = 'Error loading markers: $e';
       });
@@ -142,8 +127,7 @@ class _MyHomePageState extends State<LocationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // standart titel og baggrundsfarve
+      appBar: AppBar( // standart titel og baggrundsfarve
         title: const Text(
           'Kort over sorteringssteder',
           style: TextStyle(fontSize: 22),
@@ -168,15 +152,13 @@ class _MyHomePageState extends State<LocationPage> {
                         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                   ),
                   CurrentLocationLayer(), // loader brugeres live lokalition
-                  MarkerLayer(
-                    // tilføjer de to forskellige grupper af markør
+                  MarkerLayer( // tilføjer de to forskellige grupper af markør
                     markers: _markers1,
                   ),
                   MarkerLayer(
                     markers: _markers2,
                   ),
-                  Positioned(
-                    // tilføjer en info box der forklare hvad der er hvad
+                  Positioned( // tilføjer en info box der forklare hvad der er hvad
                     left: 6,
                     top: 6,
                     child: Container(
@@ -187,17 +169,14 @@ class _MyHomePageState extends State<LocationPage> {
                             10.0), // Juster radius for rundere hjørner
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black
-                                .withOpacity(0.3), // tilføjer skygge
+                            color: Colors.black.withOpacity(0.3), // tilføjer skygge
                             spreadRadius: 2,
                             blurRadius: 5,
-                            offset:
-                                const Offset(0, 2), // Ændring af skyggeeffekten
+                            offset: const Offset(0, 2), // Ændring af skyggeeffekten
                           ),
                         ],
                       ),
-                      child: const Row(
-                        // selve informationen insættes som standart
+                      child: const Row( // selve informationen insættes som standart
                         children: [
                           Row(
                             children: [
